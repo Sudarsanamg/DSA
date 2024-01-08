f410f0e9c5abf3c4d45e47dd0b3f4ca70a3c7dba
Sudarsanamg <97401811+Sudarsanamg@users.noreply.github.com>
2024-01-08 22:39:54 +0530

    24 pattern added

---
+++ patterns/T4.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class T4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int insp=0;
+        int outsp=2*num-2;
+       for (int i = 1; i <= 2*num; i++) {
+
+        if(i%2==0)
+        {
+            System.out.println();
+        }
+        else
+        {
+            if(i==1)
+            {
+                System.out.print('*');
+                for (int j = 1; j <= outsp; j++) {
+                    System.out.print(' ');
+                }
+                System.out.print('*');
+                outsp-=2;
+                
+            }
+            else{
+                System.out.print('*');
+                for (int j = 1; j <= insp; j++) {
+                    System.out.print(' ');
+                }
+                System.out.print('*');
+                for (int j = 1; j <= outsp; j++) {
+                    System.out.print(' ');
+                }
+                System.out.print('*');
+                for (int j = 1; j <= insp; j++) {
+                    System.out.print(' ');
+                }
+                System.out.print('*');
+                insp++;
+                outsp-=2;
+            }
+        }
+        
+       }
+       outsp=0;
+       insp--;
+       for (int i = 1; i <= 2*num; i++) {
+        if(i%2==0)
+        {
+            System.out.println();
+        }
+        else if(i==2*num-1)
+        {
+            System.out.print('*');
+            for (int j = 1; j <= outsp; j++) {
+                System.out.print(' ');
+            }
+            System.out.print('*');
+        }
+        else{
+            System.out.print('*');
+            for (int j = 1; j <=insp; j++) {
+                System.out.print(' ');
+            }
+            System.out.print('*');
+            for (int j = 1; j <= outsp; j++) {
+                System.out.print(' ');
+            }
+            System.out.print('*');
+            for (int j = 1; j <=insp; j++) {
+                System.out.print(' ');
+            }
+            System.out.print('*');
+            insp--;
+            outsp+=2;
+
+        }
+       }
+    }
+
+}
